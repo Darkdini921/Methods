4356ea874103cacaff13a9b5593383d97f56735f
Darkdini921 <chigrin.00@mail.ru>
2025-02-12 00:08:26 +0300

    homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,47 @@
+import java.time.LocalDate;
+
 public class Main {
+
+
+//    Задача 1
+
+    public static void yearLear(int year){
+        if (year % 4 == 0  && year % 100 != 0 || year % 400 == 0 && year % 100 != 0){
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
+//    Задача 2
+
+    public  static  void checkOS(int OS){
+       int clientDeviceYear = LocalDate.now().getYear();
+        if (OS == 1 && clientDeviceYear >= 2015){
+            System.out.println("Установите наше приложение для android по ссылке");
+        } if (OS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } if (OS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите наше приложение для iphone по ссылке");
+        } if (OS == 0 && clientDeviceYear < 2015){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+    }
+
+//    Задача 3
+
+    public static int deliveryDays (int distance){
+        int days = 0;
+        if (distance <= 20){
+            days = 1;
+        } if (distance > 20 && distance <= 60) {
+            days = 2;
+        } if (distance > 60 && distance <= 100){
+            days = 3;
+        }
+        return days;
+    }
+
 
 
 
@@ -6,6 +49,39 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+
+
+//        Задача 1
+        System.out.println("");
+        System.out.println("Задача 1");
+
+        int year = 2021;
+        yearLear(year);
+
+//        Задача 2
+        System.out.println("");
+        System.out.println("Задача 2");
+
+        int android = 1;
+        int iOS = 0;
+        int OS = android;
+
+        checkOS(OS);
+
+//        Задача 3
+        System.out.println("");
+        System.out.println("Задача 3");
+
+        int deliveryDistance = 10;
+        int days = deliveryDays(deliveryDistance);
+        if (deliveryDistance > 100){
+            System.out.println("Доставки нет.");
+        } else {
+            System.out.println("Потребуется дней: " + days);
+        }
+
+
 
 
 
